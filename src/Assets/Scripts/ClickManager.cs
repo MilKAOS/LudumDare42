bef1f2cfd7df5f7015ec1b59a7ba153d4c481534
Mil <michael.werlitz@gmail.com>
2018-08-12 12:49:30 +0200

    added graphics for rover, rocket, crosshair to project

--- src/Assets/Scripts/ClickManager.cs
+++ src/Assets/Scripts/ClickManager.cs
@@ -2,13 +2,18 @@
 using UnityEngine;
 
 public class ClickManager : MonoBehaviour
-{
-    public Sprite partSprite;
+{    
+    public Sprite rover1Sprite;
+    public Sprite rover2Sprite;
+    public Sprite rover3Sprite;
+    public Sprite rocketSprite;
 
     void Start()
-    {        
-        var sprites = Resources.LoadAll<Sprite>("Sprites/sprites");
-        partSprite = sprites[0];
+    {
+        rover1Sprite = Resources.Load<Sprite>("Sprites/opportunity_front_512");        
+        rover2Sprite = Resources.Load<Sprite>("Sprites/pathfinder_side_512");
+        rover3Sprite = Resources.Load<Sprite>("Sprites/spirit_front_512");
+        rocketSprite = Resources.Load<Sprite>("Sprites/falcon9_512");
     }
 
     void Update()
@@ -20,19 +25,34 @@ public class ClickManager : MonoBehaviour
             {
                 var pos = this.gameObject.transform.position;
 
-                if (this.gameObject.name.StartsWith("rover_"))
+                if (this.gameObject.name.StartsWith("rover1_"))
                 {
-                    CreatePart("part_" + Guid.NewGuid(), pos, partSprite);
-                    CreatePart("part_" + Guid.NewGuid(), pos, partSprite);
-                    CreatePart("part_" + Guid.NewGuid(), pos, partSprite);
+                    CreatePart("part_" + Guid.NewGuid(), pos, rover1Sprite);
+                    GameManager.Score(-1);
+                    GameManager.Score(1);
+                    Destroy(this.gameObject);
+                }
+                else if (this.gameObject.name.StartsWith("rover2_"))
+                {
+                    CreatePart("part_" + Guid.NewGuid(), pos, rover2Sprite);
+                    CreatePart("part_" + Guid.NewGuid(), pos, rover2Sprite);
+                    GameManager.Score(-2);
+                    GameManager.Score(1);
+                    Destroy(this.gameObject);
+                }
+                else if (this.gameObject.name.StartsWith("rover3_"))
+                {
+                    CreatePart("part_" + Guid.NewGuid(), pos, rover3Sprite);
+                    CreatePart("part_" + Guid.NewGuid(), pos, rover3Sprite);
+                    CreatePart("part_" + Guid.NewGuid(), pos, rover3Sprite);
                     GameManager.Score(-3);
                     GameManager.Score(1);
                     Destroy(this.gameObject);
                 }
                 else if (this.gameObject.name.StartsWith("rocket_"))
                 {
-                    CreatePart("part_" + Guid.NewGuid(), pos, partSprite);
-                    CreatePart("part_" + Guid.NewGuid(), pos, partSprite);
+                    CreatePart("part_" + Guid.NewGuid(), pos, rocketSprite);
+                    CreatePart("part_" + Guid.NewGuid(), pos, rocketSprite);
                     GameManager.Score(-2);
                     GameManager.Score(1);
                     Destroy(this.gameObject);
@@ -50,6 +70,7 @@ public class ClickManager : MonoBehaviour
     {
         var part = new GameObject(name);
         part.transform.position = position;
+        part.transform.localScale = new Vector3(0.1f, 0.1f, 1);
 
         SpriteRenderer renderer = part.AddComponent<SpriteRenderer>();
         renderer.sprite = sprite;
@@ -57,7 +78,7 @@ public class ClickManager : MonoBehaviour
         Rigidbody2D rigidbody2D = part.AddComponent<Rigidbody2D>();
         SetPartVelocity(rigidbody2D, position);
 
-        /*BoxCollider2D boxCollider =*/ part.AddComponent<BoxCollider2D>();
+        /*BoxCollider2D boxCollider =*/ part.AddComponent<PolygonCollider2D>();
 
         part.AddComponent<ClickManager>();
 
--- src/Assets/Scripts/GameManager.cs
+++ src/Assets/Scripts/GameManager.cs
@@ -3,21 +3,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static int PlayerScore = 1;
+    public static int PlayerScore = 0;
     public GUISkin Layout;  
 
-    public Sprite roverSprite;
+    public Sprite rover1Sprite;
+    public Sprite rover2Sprite;
+    public Sprite rover3Sprite;
     public Sprite rocketSprite;
 
     private static float secondsBetweenSpawn = 3.0f;
     private static float elapsedTime = 4.0f;
 
+    private static bool isGameStarted = false;
+
     // Use this for initialization
     void Start()
-    {
-        var sprites = Resources.LoadAll<Sprite>("Sprites/sprites");
-        roverSprite = sprites[0];
-        rocketSprite = sprites[0];
+    {        
+        rover1Sprite = Resources.Load<Sprite>("Sprites/opportunity_front_512");
+        rover2Sprite = Resources.Load<Sprite>("Sprites/pathfinder_side_512");
+        rover3Sprite = Resources.Load<Sprite>("Sprites/spirit_front_512");
+        rocketSprite = Resources.Load<Sprite>("Sprites/falcon9_512");
     }
 
     // Update is called once per frame
@@ -29,9 +34,11 @@ public class GameManager : MonoBehaviour
         {
             elapsedTime = 0;
            
-            int enemyType = UnityEngine.Random.Range(1, 3);
+            int enemyType = UnityEngine.Random.Range(1, 5);
             CreateObject(enemyType);
             Score(-1);
+
+            isGameStarted = true;
         }
     }
 
@@ -44,7 +51,7 @@ public class GameManager : MonoBehaviour
         {
             GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "HUMANITY WINS, YOU LOSE!");            
         }        
-        else if (PlayerScore >= 100)
+        else if (isGameStarted && PlayerScore == 0)
         {
             GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "MARS WINS!");
         }
@@ -64,24 +71,37 @@ public class GameManager : MonoBehaviour
     {       
         var obj = new GameObject(GetName(type));
         obj.transform.position = new Vector3(Screen.width / 2, 20, 10);
+        obj.transform.localScale = new Vector3(0.25f, 0.25f, 1);
 
         SpriteRenderer renderer = obj.AddComponent<SpriteRenderer>();
 
         switch (type)
         {
-            case 1: // Rover
-                renderer.sprite = roverSprite;
-                obj.AddComponent<JumpUpDownMovement>();
+            case 1: // Rover Opportunity
+                renderer.sprite = rover1Sprite;                
                 break;
 
-            case 2: // Rocket
+            case 2: // Rover Pathfinder
+                renderer.sprite = rover2Sprite;
+                break;
+
+            case 3: // Rover Spirit
+                renderer.sprite = rover3Sprite;
+                break;
+
+            case 4: // Rocket
                 renderer.sprite = rocketSprite;
-                obj.AddComponent<JumpAroundMovement>();
                 break;
         }
-        
-        Rigidbody2D rigidbody2D = obj.AddComponent<Rigidbody2D>();
-        BoxCollider2D boxCollider = obj.AddComponent<BoxCollider2D>();
+
+        int movementType = UnityEngine.Random.Range(1, 3);
+        if (movementType == 1)
+            obj.AddComponent<JumpUpDownMovement>();
+        else
+            obj.AddComponent<JumpAroundMovement>();
+
+        obj.AddComponent<Rigidbody2D>();
+        obj.AddComponent<PolygonCollider2D>();
 
         obj.AddComponent<ClickManager>();
 
@@ -93,8 +113,12 @@ public class GameManager : MonoBehaviour
         switch (type)
         {
             case 1: // Rover
-                return "rover_" + Guid.NewGuid();
-            case 2: // Rocket
+                return "rover1_" + Guid.NewGuid();
+            case 2: // Rover
+                return "rover2_" + Guid.NewGuid();
+            case 3: // Rover
+                return "rover3_" + Guid.NewGuid();
+            case 4: // Rocket
                 return "rocket_" + Guid.NewGuid();
             default:
                 return "blub";

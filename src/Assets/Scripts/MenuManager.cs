181d5d5781cb5f6492454926e7a91dd5d797ab79
Adrian Koch <akeean@gmail.com>
2018-08-12 21:13:52 -0300

    menu beautification

--- src/Assets/Scripts/MenuManager.cs
+++ src/Assets/Scripts/MenuManager.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
     public void ToGame()
     {
         SceneManager.LoadScene("MarsDefender");
+    }
+
+    public void ToPage()
+    {
+        Application.OpenURL("https://ldjam.com/events/ludum-dare/42/$98139");
     }
 }
